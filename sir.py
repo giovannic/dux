75596cfe517e5d7da287a1cbbf133be05542a8d8
Giovanni Charles <giovanni.charles@gmail.com>
2023-06-10 17:03:09 +0100

    Gumbel softmax implementation

--- sir.py
+++ sir.py
@@ -5,6 +5,7 @@ from jax import random, jit, grad
 from jax.lax import scan
 from jaxtyping import Array
 from jax.tree_util import register_pytree_node
+from jax.nn import softmax
 
 @dataclass
 class State:
@@ -38,6 +39,32 @@ def init(infected: float, n: int, key: Any) -> State:
         jnp.zeros((n,))
     )
 
+def _gumbel_sm_approx(
+    key: Any,
+    rate: Array,
+    shape: Tuple,
+    temp=.1,
+    min_rate=1e-10,
+    max_rate=1.
+    ) -> Array:
+    # clip rates which are incompatible
+    rate = jnp.minimum(jnp.maximum(rate, min_rate), max_rate)
+    # stack rates for p and q
+    logits = jnp.log(jnp.stack([rate, 1 - rate]))
+    # sample gumbel noise
+    gumbel = random.gumbel(key, shape)
+    # softmax -> probabilities for success and failure
+    r = softmax((logits + gumbel) / temp)
+    # return probability of success
+    return r[0]
+
+def _bernoulli(key: Any, rate: Array, shape: Tuple, **kwargs) -> Array:
+    return random.bernoulli(key, rate, shape)
+
+def bernoulli(*args, **kwargs) -> Array:
+    # return _bernoulli(*args, **kwargs)
+    return _gumbel_sm_approx(*args, **kwargs)
+
 def step(beta: float, gamma: float, state: State, key: Any) -> State:
     # get a population size
     n = state.susceptible.shape[0]
@@ -47,11 +74,11 @@ def step(beta: float, gamma: float, state: State, key: Any) -> State:
 
     # sample infections
     key, key_i = random.split(key)
-    new_infections = random.bernoulli(key_i, state.susceptible * foi, (n,))
+    new_infections = bernoulli(key_i, state.susceptible * foi, (n,))
 
     # sample recoveries
     key, key_i = random.split(key)
-    new_recoveries = random.bernoulli(key_i, state.infected * gamma, (n,))
+    new_recoveries = bernoulli(key_i, state.infected * gamma, (n,))
 
     # make new state
     return State(
@@ -95,7 +122,7 @@ def run(
 
 print(
     jit(run, static_argnums=[0, 1, 2, 3, 4])(
-        1000, .5, .35, .25, 100, random.PRNGKey(0)
+        10, .5, .35, .25, 10, random.PRNGKey(0)
     )
 )
 
@@ -103,9 +130,8 @@ def final_susceptible(*args) -> Array:
     obs = run(*args)
     return obs.n_susceptible[-1]
 
-# This will obviously not work because bernoulli is not differentiable here
 print(
     grad(final_susceptible, argnums=[2, 3])(
-        1000, .5, .5, .25, 100, random.PRNGKey(0)
+        10, .5, .5, .25, 10, random.PRNGKey(0)
     )
 )
