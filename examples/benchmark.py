Fix gumbel softmax implementation

examples/benchmark.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Callable, Iterable
+from typing import Tuple, Callable, Iterable, Union, Sequence
 from jaxtyping import Array
 from dataclasses import dataclass
 from sir import final_susceptible
@@ -18,13 +18,28 @@ methods = [
     Method('Gumbel Softmax', make_gumbel_sm_approx())
 ]
 
-def run_method(method: BernoulliSig, n: int) -> Tuple:
+def run_bernoulli(method: BernoulliSig, n: int, p: float) -> Tuple:
+    key = random.PRNGKey(0)
+    keys = random.split(key, n)
+    shape = (10000,)
+
+    def f_n_positive(k, p):
+        return 1. * jnp.sum(method(k, jnp.full(shape, p), shape))
+
+    v, g = vmap(
+        value_and_grad(f_n_positive, argnums=1),
+        in_axes=[0, None]
+    )(keys, p)
+    return v, (g,)
+
+def run_sir(method: BernoulliSig, n: int) -> Tuple:
     """
     Returns values and gradients for an SIR run
     """
     key = random.PRNGKey(0)
     keys = random.split(key, n)
     fs_with_method = partial(final_susceptible, bernoulli=method)
+
     return vmap(
         value_and_grad(fs_with_method, argnums=[2, 3, 4]),
         in_axes=[0, None, None, None, None, None]
@@ -39,15 +54,51 @@ def summarise_run(values: Array, grads: Tuple) -> Tuple:
         jnp.std(values),
     ) + tuple(_flatten((jnp.mean(g), jnp.std(g)) for g in grads))
 
-args = ['infected', 'beta', 'gamma']
-arg_headers = list(_flatten((f'Δ{a}', '(std)') for a in args))
+print('-------------------')
+print('Simple Bernoulli .3')
+print('-------------------')
 
 print(
     tabulate(
         [
-            (method.name,) + summarise_run(*run_method(method.method, 10))
+            (method.name,) + summarise_run(
+                *run_bernoulli(method.method, 10, .3)
+            )
             for method in methods
         ],
-        headers = ['value', '(std)'] + arg_headers
+        headers = ['value', '(std)', 'Δp', '(std)']
+    )
+)
+
+print('-------------------')
+print('Simple Bernoulli .7')
+print('-------------------')
+
+print(
+    tabulate(
+        [
+            (method.name,) + summarise_run(
+                *run_bernoulli(method.method, 10, .7)
+            )
+            for method in methods
+        ],
+        headers = ['value', '(std)', 'Δp', '(std)']
+    )
+)
+
+print('---')
+print('SIR')
+print('---')
+
+sir_args = ['infected', 'beta', 'gamma']
+sir_arg_headers = list(_flatten((f'Δ{a}', '(std)') for a in sir_args))
+
+print(
+    tabulate(
+        [
+            (method.name,) + summarise_run(*run_sir(method.method, 10))
+            for method in methods
+        ],
+        headers = ['value', '(std)'] + sir_arg_headers
     )
 )

src/dux/bernoulli.py
@@ -4,6 +4,7 @@ from jaxtyping import Array
 from jax.nn import softmax
 from jax import numpy as jnp
 from jax import random
+from jax.lax import stop_gradient
 
 BernoulliSig = Callable[[Any, Array, Tuple], Array]
 
@@ -30,13 +31,16 @@ def _gumbel_sm_approx(
     # clip rates which are incompatible
     rate = jnp.minimum(jnp.maximum(rate, min_rate), max_rate)
     # stack rates for p and q
-    logits = jnp.log(jnp.stack([rate, 1 - rate]))
+    logits = jnp.log(jnp.stack([1 - rate, rate]))
     # sample gumbel noise
-    gumbel = random.gumbel(key, shape)
+    gumbel = random.gumbel(key, logits.shape)
     # softmax -> probabilities for success and failure
-    r = softmax((logits + gumbel) / temp)
-    # return probability of success
-    return r[0]
+    r = softmax((logits + gumbel) / temp, axis=0)
+    # return most likely category
+    y = jnp.argmax(r, axis=0)
+    # attach the r success gradient
+    y = stop_gradient(y - r[0]) + r[0]
+    return y
 
 def bernoulli(key: Any, rate: Array, shape: Tuple) -> Array:
     return random.bernoulli(key, rate, shape)